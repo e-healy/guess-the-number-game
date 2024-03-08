55f098b4a5dc842cd31a1e9e3804f7e90aebcb68
Ellie H <elliehealy20@outlook.com>
2024-03-08 12:41:47 +0000

    Add Game class

--- src/GuessGame/GuessTheNumber.java
+++ src/GuessGame/GuessTheNumber.java
@@ -1,3 +1,43 @@
 package src.GuessGame;
 
 import java.util.Scanner;
+
+class Game {
+    private int randomNumber;
+    private final int maxRange;
+    private int numberOfAttempts;
+
+    // Game constructor
+    public Game(int maxRange, int numberOfAttempts) {
+        this.maxRange = maxRange;
+        this.numberOfAttempts = numberOfAttempts;
+        generateRandomNumber();
+    }
+
+    private void generateRandomNumber() {
+        randomNumber = (int) (Math.random() * maxRange) + 1; // Returning a number between 1 and max range
+    }
+
+    public boolean guessNumber(int guess) {
+        numberOfAttempts--;
+        if (guess == randomNumber) {
+            System.out.println("Congratulations! You guessed the correct number.");
+            return true;
+        } else if (guess < randomNumber) {
+            System.out.println("Try a higher number. Attempts left" + numberOfAttempts);
+        } else {
+            System.out.println("Try a lower number. Attempts left: " + numberOfAttempts);
+        }
+        return false;
+    }
+
+    public boolean isGameOver() {
+        return numberOfAttempts <= 0;
+    }
+
+    // Getter/Accessor
+    public int getNumber() {
+        return randomNumber;
+    }
+
+}
